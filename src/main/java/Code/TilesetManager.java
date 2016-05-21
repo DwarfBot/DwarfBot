897f0c41a79325fe8c181e9072552099c07f75f8
Alex Shafer <shafer.alex@gmail.com>
2016-05-21 12:52:15 -0400

    Make runnable jar

--- src/main/java/Code/TilesetManager.java
+++ src/main/java/Code/TilesetManager.java
@@ -269,7 +269,7 @@ public class TilesetManager extends GenericBot {
 		return null;
 	}
 	
-	private void saveImage(BufferedImage img, String title) {
+	public static void saveImage(BufferedImage img, String title) {
 		try {
 			// retrieve image
 			BufferedImage bi = img;
@@ -278,6 +278,19 @@ public class TilesetManager extends GenericBot {
 			ImageIO.write(bi, "png", outputfile);
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+	}
+
+	public void printTilesets() {
+		ArrayList<Tileset> tilesets = getTilesets();
+		for (int i = 0; i < tilesets.size(); i++) {
+			Tileset tile = tilesets.get(i);
+			System.out.println("#" + i + ": " + tile.getImagePath()
+					+ "\n\tAuthor: " + tile.getAuthor()
+					+ "\n\tWidth: " + tile.getTileWidth()
+					+ "\n\tHeight: " + tile.getTileHeight()
+					+ "\n");
+
 		}
 	}
 }
