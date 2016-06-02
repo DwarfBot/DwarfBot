79c263ef11cb8f2f2586659225ea5ab70593159e
Liam O'Flynn <lpoflynn@protonmail.com>
2016-06-01 18:32:39 -0700

    Updated scripts according to @jacobsims suggestions

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
 #Compiles code, jar-ifies it and runs it.
 #by Liam O'Flynn.
 
-./gradlew shadowJar &&
+#!/bin/bash
+set -e
+
+./gradlew shadowJar
 echo 
 echo "gradlew shadowJar completed succesfully. Executing DwarBot-all.jar..."
 echo  
-cd build/libs/ && 
+cd build/libs/
 java -jar DwarfBot-all.jar
 echo
 echo "...Complete."
